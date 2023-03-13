1aeedc63fa347cb6fb38e39383c6f6d4e328a002
Vang Huynh <vanghm@imt-soft.com>
2023-03-13 13:47:16 +0700

    update build exe

--- mybot.py
+++ mybot.py
@@ -3,6 +3,9 @@ import pyttsx3
 import openai
 import os
 from dotenv import load_dotenv
+from googletrans import Translator
+
+detector = Translator()
 
 load_dotenv()
 
@@ -23,6 +26,18 @@ while True:
 
     if you == "":
         robot_brain = "Tôi không hiểu bạn nói gì, vui lòng nói lại"
+    elif "tạm biệt" in you:
+        robot_brain = "Chúc bạn một ngày tốt lành"
+        # initial engine
+        robot_mouth = pyttsx3.init()
+        # setting voice
+        # getting details of current voice
+        voices = robot_mouth.getProperty('voices')
+        # changing index, changes voices. o for male
+        robot_mouth.setProperty('voice', voices[1].id)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
+        break
     else:
         model_engine = "gpt-3.5-turbo"
         print(you)
@@ -40,6 +55,11 @@ while True:
 
     # log robot_brain
     print(robot_brain)
+    
+    dec_lan = detector.detect(robot_brain)
+    index = 1
+    if dec_lan.lang =="en" and dec_lan.confidence == 1:
+        index = 0
 
     # initial engine
     robot_mouth = pyttsx3.init()
@@ -47,6 +67,6 @@ while True:
     # getting details of current voice
     voices = robot_mouth.getProperty('voices')
     # changing index, changes voices. o for male
-    robot_mouth.setProperty('voice', voices[1].id)
+    robot_mouth.setProperty('voice', voices[index].id)
     robot_mouth.say(robot_brain)
     robot_mouth.runAndWait()
